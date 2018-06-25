49123dcfc40c557106bf4db842e1cdfa63cd5304
Jignesh Greycube <jignesh@greycube.in>
2018-06-25 10:59:30 +0530

    Update Training Summary Report.sql

--- projects/assets/TrainingHR_Malayasia/Training Summary Report.sql
+++ projects/assets/TrainingHR_Malayasia/Training Summary Report.sql
@@ -1,15 +1,18 @@
 select 
 event_name AS "Training Event :Text:200" , 
-type as "Type:Text:120",
+type as "Type:Text:100",
 has_certificate AS "Has Certificate:Check:80",
 level AS "Level:Text:100",
-training_cost AS "Training Cost:Currency:120", 
+event_status AS "Status:Text:80", 
 total_hours AS "Total Hours:Int:80", 
-event_status AS "Status:Text:100", 
-count(TEE.name) AS "Employees Count:Int:100",
+training_cost AS "Training Cost:Currency:100", 
+(SELECT COUNT(*) from `tabTraining Event Employee` TEE where TEE.parent= TE.name) AS "Employees Count:Int:120",
+(training_cost/(SELECT COUNT(*) from `tabTraining Event Employee` TEE where TEE.parent= TE.name)) AS "Average Cost:Currency:100",
 trainer_name AS "Trainer:Text:100",
 trainer_email AS "Email:Text:100",
 contact_number AS "Contact:Text:80"
-from `tabTraining Event` TE 
-inner join `tabTraining Event Employee` TEE 
-ON TE.name = TEE.parent;
+from `tabTraining Event` TE
+where TE.docstatus= 1
+order by TE.name desc;
+
+
