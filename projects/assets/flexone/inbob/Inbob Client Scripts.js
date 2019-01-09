45384928026052b3c1adf3320e8c93df06197742
Jignesh Greycube <jignesh@greycube.in>
2019-01-09 22:32:14 +0530

    Material Request Naming series

--- projects/assets/flexone/inbob/Inbob Client Scripts.js
+++ projects/assets/flexone/inbob/Inbob Client Scripts.js
@@ -1,3 +1,26 @@
+===========================
+	Material Request
+==========================
+		frappe.ui.form.on("Material Request", "onload",
+    function(frm) {	
+	frappe.call({
+            "method": "frappe.client.get",
+            args: {
+                doctype: "User",
+		filters: {"name": frappe.user.name} ,//user is current user here
+		fieldname :["material_request_series"]  
+            },
+            callback: function (data) {
+               frm.set_value("naming_series",data.message.material_request_series);
+		
+            }
+        })
+    });
+	
+	
+
+	
+
 =========================
 	Delivery Note
 =========================
