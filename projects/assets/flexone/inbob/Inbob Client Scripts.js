97043a529f370495297c92669640ca2364dcf8ef
Jignesh Greycube <jignesh@greycube.in>
2019-01-16 22:23:09 +0530

    Update Inbob Client Scripts.js

--- projects/assets/flexone/inbob/Inbob Client Scripts.js
+++ projects/assets/flexone/inbob/Inbob Client Scripts.js
@@ -75,8 +75,14 @@ frappe.call({
 	
               if(typeof frm.doc.is_return!== 'undefined')
 		{
-		
-		 frm.set_value("naming_series",data.message.sales_return_series);// for sales return
+			if(frm.doc.is_return)
+			{
+			 frm.set_value("naming_series",data.message.sales_return_series);// for sales return
+			}
+			else
+			{	
+			 frm.set_value("naming_series",data.message.sales_invoice_series); //  for sales invoice
+			}
 		}
 		else
 		{	
@@ -89,6 +95,7 @@ frappe.call({
 	
 	
 	
+		
 ==========================================
 	Sales Order
 	=============================
@@ -164,14 +171,44 @@ frappe.ui.form.on('Sales Order Item', {
             args: {
                 doctype: "User",
 		filters: {"name": frappe.user.name} ,//user is current user here
-		fieldname :["payment_entry_series"]  
+		fieldname :["payment_entry_series","payment_pay_series"]  
             },
             callback: function (data) {
-               frm.set_value("naming_series",data.message.payment_entry_series);
+		if(frm.doc.payment_type == "Receive")
+		{
+               	frm.set_value("naming_series",data.message.payment_entry_series);
+		}
+		else
+		{
+               	frm.set_value("naming_series",data.message.payment_pay_series);
+		}
 		
             }
         })
     });
+frappe.ui.form.on("Payment Entry", "payment_type",
+    function(frm) {
+        frappe.call({
+            "method": "frappe.client.get",
+            args: {
+                doctype: "User",
+		filters: {"name": frappe.user.name} ,//user is current user here
+		fieldname :["payment_entry_series","payment_pay_series"]  
+            },
+            callback: function (data) {
+		if(frm.doc.payment_type == "Receive")
+		{
+               	frm.set_value("naming_series",data.message.payment_entry_series);
+		}
+		else
+		{
+               	frm.set_value("naming_series",data.message.payment_pay_series);
+		}
+		
+            }
+        })
+    });
+
 
 ===========================================
 	Stock Entry
